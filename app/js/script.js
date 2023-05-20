8b45225ef2b83ae0e8f4233b0e58b46a9afca120
sagitarisandy <97603275+sagitarisandy@users.noreply.github.com>
2023-05-20 19:19:12 +0700

    Add files via upload

---
+++ app/js/script.js
@@ -0,0 +1,11 @@
+const btnHamburger = document.querySelector ('#btnHamburger');
+
+btnHamburger.addEventListener('click', function(){
+    console.log('open hamburger');
+
+    if(btnHamburger.classList.contains('open')){
+        btnHamburger.classList.remove('open');
+    }else{
+        btnHamburger.classList.add('open');
+    }
+});
